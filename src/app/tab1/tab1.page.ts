5ccd9e3eb09b9bf48bf0b49df35e0de751bbf6cc
josteda99 <josteda99@gmail.com>
2022-11-01 11:29:14 -0500

    finish prototype

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
+  model = [
+    {
+      category: 'Cocer',
+      title: 'Te enseño a cocer si me enseñas a usar mi celular',
+      image:
+        'https://lanotapositiva.com/wp-content/uploads/2019/08/shutterstock_1026935038.jpg',
+    },
+    {
+      category: 'Cocina',
+      title: 'Te enseño a cocinar si me enseñas a andar en moto',
+      image:
+        'https://www.formulamoto.es/wp-content/uploads/2020/06/consejosmoto1-1.jpg.webp',
+    },
+    {
+      category: 'Matematicas',
+      title: 'Te enseño a integrar si me enseñas a tomar fotos',
+      image:
+        'https://capturetheatlas.com/wp-content/uploads/2020/04/Landscape-photography-gear.jpg',
+    },
+  ];
   constructor() {}
-
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { IonModal, ToastController } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
+  @ViewChild(IonModal) modal: IonModal;
+  user = {
+    nombre: 'Quentin Tarantulino',
+    edad: 34,
+    ocupacion: 'Estudiante de Cine',
+    avatar:
+      'https://media.revistagq.com/photos/5d39684e83c91000087972f1/16:9/w_2400,h_1350,c_limit/mejor%20pelicula%20quentin%20tarantino.jpg',
+    intereses: ['cocina', 'arte', 'motos'],
+    ense: ['fotografia', 'composicion', 'edicion'],
+  };
 
-  constructor() {}
+  message =
+    'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
 
+  constructor(private toastController: ToastController) {}
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.presentToast();
+    }
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Anuncio creado con exito!',
+      duration: 1500,
+      position: 'bottom',
+      color: 'success',
+    });
+
+    await toast.present();
+  }
 }
